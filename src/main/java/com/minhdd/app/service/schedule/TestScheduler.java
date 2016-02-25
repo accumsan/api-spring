3e59f70375b939c9ec291998aed0c47b16d6dd35
minhdao <md.insa@gmail.com>
2016-02-25 22:58:12 +0100

    desactivate test schedule

--- src/main/java/com/minhdd/app/service/schedule/TestScheduler.java
+++ src/main/java/com/minhdd/app/service/schedule/TestScheduler.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TestScheduler {
-    @Scheduled(fixedDelayString = "${test.delay}")
+    //@Scheduled(fixedDelayString = "${test.delay}")
     public void demoServiceMethod(){
         System.out.println("this method is called at every ${test.delay}/1000 seconds");
     }
-    @Scheduled(cron = "0 56 22 * * ?")
+    //@Scheduled(cron = "0 56 22 * * ?")
     public void cron1(){
         System.out.println("this method is called at 22:56 everyday");
     }
-    @Scheduled(cron = "${test.cron.expression}")
+    //@Scheduled(cron = "${test.cron.expression}")
     public void cron()
     {
         System.out.println("Method scheduled by cron set by property test.cron.expression");
