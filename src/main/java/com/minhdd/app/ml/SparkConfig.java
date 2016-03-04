5a8e56d8891910c88c63687a25d94210872893b2
Minh DAO <minh.dao@ext.quicksign.com>
2016-03-04 14:21:15 +0100

    ml >> beanify sql context

--- src/main/java/com/minhdd/app/ml/SparkConfig.java
+++ src/main/java/com/minhdd/app/ml/SparkConfig.java
@@ -1,14 +1,17 @@
 package com.minhdd.app.ml;
 
+import com.minhdd.app.config.Constants;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SQLContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by mdao on 04/03/2016.
  */
 @Configuration
+@Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
 public class SparkConfig {
 
     @Bean
